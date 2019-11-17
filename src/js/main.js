cd6dc5acf1ae9373fd705b3bb82acc7cc18fa253
peacemaxpriya <52803079+peacemaxpriya@users.noreply.github.com>
2019-11-16 20:41:06 -0500

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -10,17 +10,13 @@ languageButtons.forEach(function(languageButton) {
 	})
 }) 
 
-// cookie check for italian
-if (document.cookie.includes('language=italian')) {
-	console.log('Welcome to italian.')
-}
 
 // hide next button until called
-// let nextButton = document.querySelector('.next')
-// nextButton.style.display = 'none'
+let nextButton = document.querySelector('.next')
+nextButton.style.display = 'none'
 
 
-// italian quiz
+// italian quiz array
 let italianSwearWords = [
 	{
 		question: 'I hope you mangia merde e morte!',
@@ -56,34 +52,86 @@ let italianSwearWords = [
 	// },
 ]
 
+// japanese quiz array 
+let japaneseSwearWords = [
+	{
+		question: 'Could you shi\'ne kutabare?',
+		answers: {
+			a: 'go fuck yourself',
+			b: 'go to hell',
+			c: 'please die', // correct
+		},
+		correct: 'c',
+	},
+	{	
+		question: 'Why don\'t you damare?',
+		answers: {
+			a: 'dig yourself a grave',
+			b: 'shut your fucking face', // correct
+			c: 'go eat ramen',
+		},
+		correct: 'b',
+	},
+]
+
+
+// generic code for the quiz
 let options = document.querySelector('.options')
 let question = document.querySelector('.question')
 let currentQuestion = 0
 
-let italianQuiz = function() {
-	question.innerText = italianSwearWords[currentQuestion].question
-	for (let option in italianSwearWords[currentQuestion].answers) {
+let quiz = function(swearWords) {
+	question.innerText = swearWords[currentQuestion].question
+	for (let option in swearWords[currentQuestion].answers) {
 		let eachOption = document.createElement('button')
-		eachOption.innerText = italianSwearWords[currentQuestion].answers[option]
+		eachOption.innerText = swearWords[currentQuestion].answers[option]
 		options.appendChild(eachOption)
 		eachOption.setAttribute('id', option)
 		console.log(eachOption.id)
-		
+
 		eachOption.addEventListener('click', function() {
-			if (eachOption.id === (italianSwearWords[currentQuestion].correct)) {
+			if (eachOption.id === (swearWords[currentQuestion].correct)) {
 				eachOption.setAttribute('id', 'right')
 				document.querySelector('#right').style.backgroundColor = 'green'
 			} else {
 				eachOption.setAttribute('class', 'wrong')
 				document.querySelector('.wrong').style.backgroundColor = 'red'
 			}
-			// nextButton.style.display = 'block'
-			// nextButton.style.backgroundColor = '#fff'
+			nextButton.style.display = 'block'
+			nextButton.style.backgroundColor = '#fff'
 		})
 	}
 }
 
-italianQuiz()
+// go to the next question
+let nextQuestion = function(swearWords) {
+	nextButton.addEventListener('click', function() {
+		question.innerText = ' '
+		options.innerHTML = ' '
+
+		currentQuestion += 1
+
+		if (currentQuestion === 2) {
+			document.querySelector('.learn-to-swear').innerHTML = 'Great Job'
+		} else {
+			quiz(swearWords)
+		}
+		nextButton.style.display = 'none'
+	})
+}
+
+// get the quiz to start and go to the next question
+if (document.cookie.includes('language=italian')) {
+	console.log('Welcome to italian.')
+	quiz(italianSwearWords)
+	nextQuestion(italianSwearWords)
+} else if (document.cookie.includes('language=japanese')) {
+	console.log('Welcome to japanese.')
+	quiz(japaneseSwearWords)
+	nextQuestion(japaneseSwearWords)	
+}
+
+
 
 
 
