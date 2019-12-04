41926446ea063b6b2e55793d6d9f0c308de73163
peacemaxpriya <52803079+peacemaxpriya@users.noreply.github.com>
2019-12-04 11:10:55 -0500

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -282,32 +282,32 @@ let nextQuestion = function(swearWords) {
 }
 
 // certificate page 
-let certificateToShare = document.createElement('meta')
-certificateToShare.setAttribute('property', 'og:image')
-certificateToShare.setAttribute('content', ' ')
-document.querySelector('head').appendChild(certificateToShare)
+// let certificateToShare = document.createElement('meta')
+// certificateToShare.setAttribute('property', 'og:image')
+// certificateToShare.setAttribute('content', ' ')
+// document.querySelector('head').appendChild(certificateToShare)
 
 // get the quiz to start and go to the next question
 if (document.cookie.includes('language=italian')) {
 	quiz(italianSwearWords)
 	nextQuestion(italianSwearWords)
 	document.querySelector('.cert').setAttribute('src', 'dist/img/certificates/italian-cert.png')
-	document.querySelector('.cert-to-share').setAttribute('content', 'dist/img/certificates/italian-cert.png')
+	document.querySelector('.cert-to-share').setAttribute('content', 'https://peacemaxpriya.github.io/duolingo-dark/dist/img/certificates/italian-cert.png')
 } else if (document.cookie.includes('language=japanese')) {
 	quiz(japaneseSwearWords)
 	nextQuestion(japaneseSwearWords)
 	document.querySelector('.cert').setAttribute('src', 'dist/img/certificates/japanese-cert.png')
-	document.querySelector('.cert-to-share').setAttribute('content', 'dist/img/certificates/japanese-cert.png')	
+	document.querySelector('.cert-to-share').setAttribute('content', 'https://peacemaxpriya.github.io/duolingo-dark/dist/img/certificates/japanese-cert.png')	
 } else if (document.cookie.includes('language=german')) {
 	quiz(germanSwearWords)
 	nextQuestion(germanSwearWords)
 	document.querySelector('.cert').setAttribute('src', 'dist/img/certificates/german-cert.png')
-	document.querySelector('.cert-to-share').setAttribute('content', 'dist/img/certificates/german-cert.png')	
+	document.querySelector('.cert-to-share').setAttribute('content', 'https://peacemaxpriya.github.io/duolingo-dark/dist/img/certificates/german-cert.png')	
 } else if (document.cookie.includes('language=chinese')) {
 	quiz(chineseSwearWords)
 	nextQuestion(chineseSwearWords)	
 	document.querySelector('.cert').setAttribute('src', 'dist/img/certificates/chinese-cert.png')
-	document.querySelector('.cert-to-share').setAttribute('content', 'dist/img/certificates/chinese-cert.png')
+	document.querySelector('.cert-to-share').setAttribute('content', 'https://peacemaxpriya.github.io/duolingo-dark/dist/img/certificates/chinese-cert.png')
 } 
 
 
